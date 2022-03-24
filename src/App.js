019a2b1cfc04ea940a18ff2560a665cad891338b
ahasimon <102286115+ahasimon@users.noreply.github.com>
2022-03-24 22:53:44 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -77,6 +77,7 @@ export const StyledLogo = styled.img`
 export const StyledImg = styled.img`
   box-shadow: 0px 5px 11px 2px rgba(0, 0, 0, 0.7);
   border: 4px solid var(--secondary);
+  border-color: white;
   background-color: var(--accent);
   border-radius: 100%;
   width: 200px;
